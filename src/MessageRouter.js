62ef12c4ec0b7a01661ae931d7c96770f4295443
Evan Dorn <evan@lrdesign.com>
2016-09-30 20:03:27 -0700

    Starting progress on message routing with observer pattern

--- src/MessageRouter.js
+++ src/MessageRouter.js
@@ -1,13 +1,33 @@
 import { Dependencies } from 'constitute';
+import { _ } from "underscore";
+// import WebsocketHandler from 'WebsocketHandler';
+// import DevUX from "DevUX";
 
-import WebsocketHandler from 'WebsocketHandler';
-import DevUX from "DevUX";
 
+// Handles routing messages from the websocket to modules that care
+// about them.
 
-@Dependencies(WebsocketHandler, DevUX)
-export class MessageRouter {
+@Dependencies()
+export default class MessageRouter {
   constructor(websocketHandler, devUX){
-    this.websocketHandler = websocketHandler;
-    this.devUX = devUX;
+    this.subscriptions = new Map();
   };
+
+  subscribe(key, callback) {
+    this.subscriptions.set(key, callback);
+    console.log(this.subscriptions);
+  }
+  unsubscribe(messageKey, callback) {
+    delete this.subscriptions.get(messageKey);
+  }
+
+  // Pass this message to everyone subscribing to one of its keys.
+  handle(message) {
+    this.subscriptions.forEach((callback, key) => {
+      if (message[key]) {
+        callback.call(null, message);
+      }
+    });
+  }
+
 }
--- test/MessageRouterTest.js
+++ test/MessageRouterTest.js
@@ -1,8 +1,28 @@
-import { chai } from 'chai';
+// import sinon from 'sinon';
 
+import MessageRouter from "MessageRouter";
 
-describe("Tests", () => {
-  it("works", () => {
-    expect(true).to.be.true;
+console.log(MessageRouter);
+
+describe("Passing messages to registered callbacks", () => {
+  it("passes messages to a callback registered for that key", () => {
+    var router = new MessageRouter();
+    var listener = sinon.spy();
+    var message = { foo: 1};
+
+    router.subscribe("foo", listener);
+    router.handle(message);
+    expect(listener).to.have.been.calledWith(message);
   });
+
+  it("doesn't pass non-matching messages to a callback", () => {
+    var router = new MessageRouter();
+    var listener = sinon.spy();
+    var message = { foo: 1};
+
+    router.subscribe("bar", listener);
+    router.handle(message);
+    expect(listener).not.to.have.been.called;
+  });
+
 });
